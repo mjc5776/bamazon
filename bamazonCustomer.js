314b84e19f7e66ac921d1ab5f676c168221d4010
MCorey <mjcorey57@gmail.com>
2019-01-16 22:51:32 -0700

    Part 1 working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,67 +9,102 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+
 listProducts();
 
-
 function listProducts() {
-    //queryStr = "SELECT item_id, product_name, department_name, price FROM products"
-    connection.query("SELECT item_id, product_name, department_name, price FROM products", function (err, results) {
+    queryStr = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products"
+    connection.query(queryStr, function (err, results) {
         if (err) throw err;
         console.log("\n\n");
         console.log('BAMAZON');
-        
+
         var t = new Table;
-        //console.log(products);
-        
-        results.forEach(function(product) {
+
+        results.forEach(function (product) {
             t.cell('Item', product.item_id);
             t.cell('Product', product.product_name);
             t.cell('Department', product.department_name);
             t.cell('Price', product.price);
-            t.cell('Qty'), product.stock_quantity 
-            t.newRow();   
-        }); 
-       console.log(t.toString());
-       
+            t.cell('Qty', product.stock_quantity);
+            t.newRow();
         });
-    };
+        console.log(t.toString());
+        productToBuy();
+    });
+
+};
 
 
-    //productToBuy();
-   
-    function productToBuy() {
-        inquirer
-            .prompt([{
-                    name: "id",
-                    type: "input",
-                    message: "What is the ID of the product you want to purchase?",
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
+
+function productToBuy() {
+    inquirer
+        .prompt([{
+                name: "id",
+                type: "input",
+                message: "What is the ID of the product you want to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
                     }
-                },
-                {
-                    name: "qty",
-                    type: "input",
-                    message: "How many units would you like to buy?",
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
-                    }
+                    return false;
                 }
-            ])
-            .then(function (answer) {
-                var query = "SELECT item_id as ProductID, stock_quantity FROM products WHERE ?"
-                connection.query(query, {
-                    item_id: answer.id
-                }, function (err, res) {
-                    console.log(res);
+            },
+            {
+                name: "qty",
+                type: "input",
+                message: "How many units would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answer) {
+            var query = "SELECT * FROM products WHERE ?"
+            connection.query(query, {
+                item_id: answer.id
+            }, function (err, res) {
+                
+                if (res[0].stock_quantity < answer.qty) {
+                    console.log("Insufficient Quantity");
+                    listProducts();
+                } else {
+                    
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                              stock_quantity: res[0].stock_quantity - answer.qty  
+                            },
+                            {
+                                item_id: answer.id
+                            }
 
-                })
-            })
-    };
+                        ],
+                        function(error){
+                            if (error) throw err;
+                            var orderTotal = (answer.qty * res[0].price)
+                            console.log("Order Complete. Your total is: $" + orderTotal);
+                            
+                            closeConnection();
+                        }
+                    )   
+                    
+                    
+                }
+
+
+            });
+        });
+
+
+
+};
+
+
+
+function closeConnection() {
+    connection.end();
+};
